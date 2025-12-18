c8f780c8048552fda79e326b84767debfa9697f5
valentincostea180 <152385738+valentincostea180@users.noreply.github.com>
2025-12-18 17:06:36 +0200

    outlined future schematic of site, added initial logic for data input

--- store_deliv/src/App.tsx
+++ store_deliv/src/App.tsx
@@ -3,28 +3,149 @@ import "./App.css";
 
 function App() {
   const [showNewProduct, setShowNewProduct] = useState(false);
+  const [showOldProduct, setShowOldProduct] = useState(true);
+  const [showNewLocation, setShowNewLocation] = useState(false);
+  const [showOldLocation, setShowOldLocation] = useState(true);
+
+  const [newProduct, setNewProduct] = useState({
+    photo: "",
+    name: "",
+    quantity: "",
+  });
+
+  const [newLocation, setNewLocation] = useState({
+    name: "",
+    address: "",
+  });
 
   return (
     <>
       <div className="agenda-header">
-        <h2>Company Deliverables System</h2>
+        <h1>Company Deliverables System</h1>
       </div>
 
       <div className="agenda-content">
-        {showNewProduct && <div className="agenda-panel"> 1 </div>}
+        <div className="meetings-panel">
+          {showOldProduct && !showNewProduct && (
+            <div>
+              <h2> There are no deliverable saved.</h2>
+              <button
+                className="btn-tsx"
+                onClick={() => {
+                  setShowNewProduct(true);
+                }}
+              >
+                ADD
+              </button>
+            </div>
+          )}
 
-        <div className="meetings-panel"></div>
+          {showNewProduct && (
+            <div>
+              <h2>Add New Deliverable</h2>
+              <div className="row g-2">
+                <div className="col-md">
+                  <div className="form-floating">
+                    <input
+                      type="text"
+                      placeholder="Photo"
+                      value={newProduct.name}
+                      onChange={(e) =>
+                        setNewProduct({ ...newProduct, photo: e.target.value })
+                      }
+                      className="form-control"
+                    />
+                  </div>
+                </div>
 
-        <button
-          className="btn-tsx"
-          onClick={() => {
-            setShowNewProduct(true);
-          }}
-        >
-          +
-        </button>
+                <div className="col-md">
+                  <div className="form-floating">
+                    <input
+                      type="text"
+                      placeholder="Product name"
+                      value={newProduct.name}
+                      onChange={(e) =>
+                        setNewProduct({ ...newProduct, name: e.target.value })
+                      }
+                      className="form-control"
+                    />
+                  </div>
+                </div>
 
-        <div className="meetings-panel"></div>
+                <div className="col-md">
+                  <div className="form-floating">
+                    <input
+                      type="text"
+                      placeholder="Quantity"
+                      value={newProduct.quantity}
+                      onChange={(e) =>
+                        setNewProduct({
+                          ...newProduct,
+                          quantity: e.target.value,
+                        })
+                      }
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="meetings-panel">
+          {showOldLocation && !showNewLocation && (
+            <div>
+              <h2> There are no deliverable saved.</h2>
+              <button
+                className="btn-tsx"
+                onClick={() => {
+                  setShowNewLocation(true);
+                }}
+              >
+                ADD
+              </button>
+            </div>
+          )}
+
+          {showNewLocation && (
+            <div>
+              <h2>Add New Location</h2>
+              <div className="row g-2">
+                <div className="col-md">
+                  <div className="form-floating">
+                    <input
+                      type="text"
+                      placeholder="Address"
+                      value={newLocation.name}
+                      onChange={(e) =>
+                        setNewLocation({
+                          ...newLocation,
+                          address: e.target.value,
+                        })
+                      }
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-md">
+                  <div className="form-floating">
+                    <input
+                      type="text"
+                      placeholder="Location name"
+                      value={newLocation.name}
+                      onChange={(e) =>
+                        setNewLocation({ ...newLocation, name: e.target.value })
+                      }
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
