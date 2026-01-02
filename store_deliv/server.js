5e00ed0cd006fa098481ee05bf70f71c2240c8d0
valentincostea180 <152385738+valentincostea180@users.noreply.github.com>
2026-01-02 21:37:11 +0000

    added fundamental part of server side

--- store_deliv/server.js
+++ store_deliv/server.js
@@ -4,3 +4,10 @@ import express from "express";
 // create __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
