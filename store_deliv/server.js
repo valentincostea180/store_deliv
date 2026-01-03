cb5a5dda98041333a47717761b2a94b034b43aea
valentincostea180 <152385738+valentincostea180@users.noreply.github.com>
2026-01-03 23:29:00 +0200

    added new serverside logic, checks that the data exists

--- store_deliv/server.js
+++ store_deliv/server.js
@@ -4,19 +4,8 @@ import express from "express";
 // create __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-<<<<<<< HEAD
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-=======
-
-const app = express();
-<<<<<<< HEAD
 const PORT = process.env.PORT || 5000;
 
 // Middleware
