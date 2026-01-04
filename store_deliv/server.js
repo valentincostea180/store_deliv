e5d98df1e1cd103e8e90300c11ee0b17e5883104
valentincostea180 <152385738+valentincostea180@users.noreply.github.com>
2026-01-04 21:08:50 +0000

    eliminated git attributes errors

--- store_deliv/server.js
+++ store_deliv/server.js
@@ -4,15 +4,22 @@ import express from "express";
 // create __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+<<<<<<< HEAD
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+=======
 
 const app = express();
 <<<<<<< HEAD
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
-=======
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files from public directory
@@ -22,4 +29,3 @@ const dataDir = path.join(__dirname, "public", "data");
 if (!fs.existsSync(dataDir)) {
   fs.mkdirSync(dataDir, { recursive: true });
 }
->>>>>>> 31ca16b (added new serverside logic, checks that the data exists)
